c26e0cc0dad687e0325aae04a782e187c2c494c5
Oksana <n.ksenechka.v@gmail.com>
2026-07-25 13:02:17 +0200

    Lesson 1: variables and data types

---
+++ lesson-1.js
@@ -0,0 +1,21 @@
+const firstName = "Oksana";
+const age = 39;
+const country = "Germany";
+const isDeveloper = true;
+
+console.log (typeof firstName);
+console.log (typeof age);
+console.log (typeof country);
+console.log (typeof isDeveloper);
+
+const profession = "Frontend Developer";
+const city = "Kettenkamp";
+const favouriteColor = "Blue";
+const hasCar = false;
+const children = 1;
+
+console.log (typeof profession);
+console.log (typeof city);
+console.log (typeof favouriteColor);
+console.log (typeof hasCar);
+console.log (typeof children);
