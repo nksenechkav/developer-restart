bd03b73163683a04f4a4b1030f5d4558e3a90274
Oksana <n.ksenechka.v@gmail.com>
2026-07-27 12:36:46 +0200

    Lesson 4: comparison operators

---
+++ lesson-4.js
@@ -0,0 +1,18 @@
+const age = 18;
+
+console.log (age > 16);
+console.log (age < 16);
+console.log (age >= 18);
+console.log (age <= 18);
+console.log (age === 18);
+console.log (age !== 18);
+
+const a = 10;
+const b = "10";
+
+console.log(a === b);
+console.log(a !== b);
+console.log(a > 5);
+console.log(b === "10");
+console.log(typeof a);
+console.log(typeof b);
