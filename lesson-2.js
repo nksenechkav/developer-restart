3b656ee362096b54030e738804a02c707fc4d7c1
Oksana <n.ksenechka.v@gmail.com>
2026-07-27 11:36:19 +0200

    Lesson 2: Personal Info

---
+++ lesson-2.js
@@ -0,0 +1,10 @@
+const firstName = "Oksana";
+const lastName = "Nesteruk";
+const age = 40;
+const fullName = firstName + " "+ lastName;
+const personalInfo = `My name is ${fullName}. I am ${age} years old.`; 
+
+console.log (firstName);
+console.log (lastName);
+console.log (fullName);
+console.log (personalInfo);
