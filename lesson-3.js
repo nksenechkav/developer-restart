3d6f3529908a851c714463d42c31c966f745aec7
Oksana <n.ksenechka.v@gmail.com>
2026-07-27 12:15:34 +0200

    Lesson 3: arithmetic

---
+++ lesson-3.js
@@ -0,0 +1,26 @@
+//1-st Task//
+
+const a = 15;
+const b = 5;
+
+console.log (`${a} + ${b} = ${a + b}`);
+console.log (`${a} - ${b} = ${a - b}`);
+console.log (`${a} * ${b} = ${a * b}`);
+console.log (`${a} / ${b} = ${a / b}`);
+
+// 2-st Task//
+
+const price = 25;
+const quantity = 4;
+const totalPrice = price * quantity;
+
+console. log (`Total price = ${totalPrice}`);
+
+// 3-st Task//
+
+const originalPrice = 100;
+const discount = 20;
+const discountAmount = (originalPrice * discount) / 100;
+
+console.log (`Discount = ${discountAmount}`);
+console.log (`Final Price = ${originalPrice - discountAmount}`);
