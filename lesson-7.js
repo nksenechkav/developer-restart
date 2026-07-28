1a148611434a1d05ac4832e5faab51edf7cb4967
Oksana <n.ksenechka.v@gmail.com>
2026-07-28 12:54:42 +0200

    Lesson-7: logical operators

---
+++ lesson-7.js
@@ -0,0 +1,16 @@
+const age = 25;
+const hasPassport = true;
+
+
+if (age >= 18 && hasPassport) {
+    console.log ("You can travel");
+} else {
+    console.log ("You cannot travel");
+}
+
+const hasTicket = false;
+const isVIP = true;
+
+if (hasTicket || isVIP) {
+    console.log ("You may enter")
+}
