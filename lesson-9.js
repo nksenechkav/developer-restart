d2d212f2f1a437a953172be2fca7101efa5b6c51
Oksana <n.ksenechka.v@gmail.com>
2026-07-30 10:50:50 +0200

    Lesson-9: Funktions

---
+++ lesson-9.js
@@ -0,0 +1,16 @@
+function sayHello (name) {
+    console.log (`Hello, ${name}!`);
+}
+
+sayHello ("Oksana");
+sayHello ("Anna");
+sayHello ("John");
+
+
+function add (a,b) {
+    return a + b;
+}
+
+const result = add (5, 3);
+
+console.log (result);
