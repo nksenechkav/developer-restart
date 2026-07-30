fd10cd6b95aeae8e8975e0c129141ca002b7917c
Oksana <n.ksenechka.v@gmail.com>
2026-07-30 12:37:41 +0200

    Lesson 11: Arrays

---
+++ lesson-11.js
@@ -0,0 +1,15 @@
+const fruits = ["apple", "banana", "orange"];
+
+console.log (fruits[0]);
+console.log (fruits[1]);
+console.log (fruits[2]);
+
+console.log (fruits.length);
+
+fruits.pop ();
+
+console.log (fruits);
+
+fruits.push ("Orange");
+
+console.log (fruits);
