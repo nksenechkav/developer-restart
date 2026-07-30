81199841a2cf276c72729c74e38b0d8389bed03d
Oksana <n.ksenechka.v@gmail.com>
2026-07-30 13:06:19 +0200

    Lesson 12: Arrays-forEach

---
+++ lesson-12.js
@@ -0,0 +1,10 @@
+const users = ["Oksana", "Anna", "John"]; 
+
+users.forEach (function (user){
+    console.log (`Hello, ${user}`);
+});
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.forEach (function (number) {
+    console.log (`Number: ${number}`);  
+});
