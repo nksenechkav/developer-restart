c052aef7395f1d20f197c57a029a2f636cccb3a3
Oksana <n.ksenechka.v@gmail.com>
2026-08-03 12:31:31 +0200

    Lesson-16: Arrow Functions

---
+++ lesson-16.js
@@ -0,0 +1,32 @@
+//Task1//
+
+const multiply = (a, b) => a * b;
+
+const result = multiply (6,7);
+
+console.log (result);
+
+//Task2//
+
+const greet = name => `Hello, ${name}!`;
+
+const welcome = greet ("Oksana");
+
+console.log (welcome);
+
+//Task3//
+
+const square = number => number*number;
+
+const result2 = square (4);
+
+console.log (result2);
+
+//Task4//
+
+const numbers = [1, 2, 3, 4, 5];
+
+const doubled = numbers.map (number => number * 2);
+
+
+console.log(doubled);
