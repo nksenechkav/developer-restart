947bdeba5658c64a658735d1a32322ae17c7952e
Oksana <n.ksenechka.v@gmail.com>
2026-09-18 20:47:30 +0200

    Lesson-43: try... catch

---
+++ lesson-43.js
@@ -0,0 +1,21 @@
+//Task1//
+
+try {
+  console.log("Начало");
+  someFunction();
+  console.log("Конец");
+} catch (error) {
+  console.log("Произошла ошибка");
+}
+
+//Task2//
+
+try {
+  console.log("A");
+  console.log(10 / 2);
+  console.log("B");
+} catch (error) {
+  console.log("Ошибка");
+}
+
+console.log("C");
